fa8ea7f5c44bcc535356e454f6f11bcb3b13ad95
Shivendra <shivendrakumarsahu2@gmail.com>
2021-05-24 16:33:18 +0530

    Init commit

---
+++ public/JS/app.js
@@ -0,0 +1,28 @@
+console.log('Client side JSS file is loaded.')
+
+const weatherform = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messagetwo = document.querySelector('#message-2')
+
+weatherform.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    messageOne.textContent = 'Loading weather info please wait..'
+    messagetwo.textContent = ''
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                
+                messageOne.textContent =data.error
+            }
+            else {
+                messageOne.textContent = data.Location
+                messagetwo.textContent = data.forecast
+            }
+        })
+    })
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('postman-request')
+
+const forecast = (lat, long, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=a915608d584b5ef16dbe8c0ecab26cff&query=' + lat + ',' + long + '&units=f'
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service.', undefined)
+        }
+        else if (body.error) {
+            callback('Unable to find location.Please enter valid coordinates', undefined)
+        }
+        else {
+            callback(undefined, 'The weather condition is : ' + body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + ' degrees though.')
+        }
+    })
+}
+module.exports = forecast
