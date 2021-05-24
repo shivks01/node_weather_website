189f77e2af29de7122d53c2f89841b9bbbcf510c
Shivendra <shivendrakumarsahu2@gmail.com>
2021-05-24 16:35:39 +0530

    Remove unnecessary console.log calls

--- public/JS/app.js
+++ public/JS/app.js
@@ -1,5 +1,3 @@
-console.log('Client side JSS file is loaded.')
-
 const weatherform = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
